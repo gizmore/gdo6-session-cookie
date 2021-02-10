640c86a194ae88f5ea90d5fd55db8407ade04345
gizmore <gizmore@wechall.net>
2021-02-10 06:19:46 +0100

    6.10-r9483_seo_urls

--- GDO_Session.php
+++ GDO_Session.php
@@ -2,7 +2,6 @@
 namespace GDO\Session;
 
 use GDO\Core\Application;
-use GDO\Core\Logger;
 use GDO\User\GDO_User;
 use GDO\Util\Math;
 use GDO\Util\AES;
@@ -295,7 +294,7 @@ class GDO_Session
         }
         $this->cookieData['sess_time'] = Application::$TIME;
         $json = json_encode($this->cookieData);
-        Logger::logDebug($json);
+//         Logger::logDebug($json);
 //         $encoded = zlib_encode($json, ZLIB_ENCODING_GZIP, 9);
 //         $encrypted = AES::encryptIV($encoded, GWF_SALT);
         $encrypted = AES::encryptIV($json, GWF_SALT);
